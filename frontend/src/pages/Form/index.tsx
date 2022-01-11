8996b99d0ee908bf12a41353523dd1bb8f48e571
victorhttps <dev.victorhugo@gmail.com>
2022-01-11 19:40:14 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,10 @@
+function Listing() {
+  return (
+    <div>
+      <h1>Pagina de Formulario</h1>
+      <h3>Formulario</h3>
+    </div>
+  );
+}
+
+export default Listing;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,10 @@
+function Listing() {
+  return (
+    <div>
+      <h1>Pagina de listagem</h1>
+      <h3>Listas</h3>
+    </div>
+  );
+}
+
+export default Listing;

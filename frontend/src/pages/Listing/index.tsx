f6bdab2d29865ce2e909b0ef336312c69d71bc66
Victor Hugo <76017410+victorhttps@users.noreply.github.com>
2022-01-16 01:03:14 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,38 +6,42 @@ import { MoviePage } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
 
 function Listing() {
-
-const [pageNumber, setPageNumber] = useState(0);
-const [page, setPage] = useState<MoviePage>({
-  content: [],
-  last: true,
-  totalPages: 0,
-  totalElements: 0,
-  size: 12,
-  number: 0,
-  first: true,
-  numberOfElements: 0,
-  empty: true,
-});
-
-useEffect(() => {
-  axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
-  .then(reponse => {
-    const data = reponse.data as MoviePage;
-    setPage(data);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [page, setPage] = useState<MoviePage>({
+    content: [],
+    last: true,
+    totalPages: 0,
+    totalElements: 0,
+    size: 12,
+    number: 0,
+    first: true,
+    numberOfElements: 0,
+    empty: true,
   });
-}, [pageNumber])
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+      .then((reponse) => {
+        const data = reponse.data as MoviePage;
+        setPage(data);
+      });
+  }, [pageNumber]);
+
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  };
 
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
-          {page.content.map(movie =>(
+          {page.content.map((movie) => (
             <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-            <MovieCard movie={movie}/>
-          </div>)
-          )}
+              <MovieCard movie={movie} />
+            </div>
+          ))}
         </div>
       </div>
     </>
